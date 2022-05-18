a06ae2952c6d492139197bce33a5cfc5a1354784
dix0nym <dixonym@mailbox.org>
2022-05-18 23:09:00 +0200

    Add info text for header and cookies

--- js/chrome_config.js
+++ js/chrome_config.js
@@ -4,12 +4,13 @@ const chrome_config = {
             info: 'During startup and browsing domain names are getting pre-resolved. This ranges from sites you currently visit to pages you might look up later, or some of the suggested URL while typing. Therefore your browsing habbits are getting passed as information.',
             recommendedValue: false,
             warningLevel: 'high'
-        },
-        webRTCIPHandlingPolicy: {
-            info: 'WebRTC helps with the real-time-communication, but is known for leaking your local address depending on usage. Therefore use it with caution.',
-            recommendedValue: false,
-            warningLevel: 'high'
         }
+        // TODO: currently only TRUE/FALSE settings supported
+        // webRTCIPHandlingPolicy: {
+        //     info: 'WebRTC helps with the real-time-communication, but is known for leaking your local address depending on usage. Therefore use it with caution.',
+        //     recommendedValue: false,
+        //     warningLevel: 'high'
+        // }
     },
     services: {
         alternateErrorPagesEnabled: {
@@ -97,60 +98,54 @@ const chrome_config = {
     }
 };
 
+// source: https://owasp.org/www-project-secure-headers/#div-headers
 const security_headers = {
     "x-frame-options": {
         warningLevel: 'high',
-        info: 'important header, should always set'
-    },
-    "x-xss-protection": {
-        warningLevel: 'high',
-        info: 'important header, should always set'
+        info: 'rotection of web applications against clickjacking. It instructs the browser whether the content can be displayed within frames.'
     },
     "content-security-policy": {
         warningLevel: 'high',
-        info: 'important header, should always set'
+        info: 'CSP prevents a wide range of attacks, including cross-site scripting and other cross-site injections.'
     },
     "strict-transport-security": {
         warningLevel: 'high',
-        info: 'important header, should always set'
+        info: 'Header protects websites against protocol downgrade attacks and cookie hijacking. It allows web servers to declare that clients should only interact with it using secure HTTPS connections, and never via the insecure HTTP protocol.'
     },
     "x-content-type-options": {
-        warningLevel: 'high',
-        info: 'important header, should always set'
+        warningLevel: 'medium',
+        info: 'Header will prevent the browser from interpreting files as a different MIME type to what is specified in the Content-Type HTTP header'
     },
     "x-permitted-cross-domain-policies": {
         warningLevel: 'high',
-        info: 'important header, should always set'
+        info: 'A cross-domain policy file is an XML document that grants a web client permission to handle data across domains'
     },
     "referrer-policy": {
         warningLevel: 'high',
-        info: 'important header, should always set'
+        info: 'Header governs which referrer information, sent in the Referer header, should be included with requests made.'
     },
-    "expect-ct": {
-        warningLevel: 'high',
-        info: 'important header, should always set'
-    },
-    "feature-policy": {
-        warningLevel: 'high',
-        info: 'important header, should always set'
+    "Permissions-Policy": {
+        warningLevel: 'medium',
+        info: 'HTTP header can be used in the response (server to client) to communicate the permissions policy that should be enforced by the client.'
     }
 };
 
+// source: https://owasp.org/www-project-web-security-testing-guide/latest/4-Web_Application_Security_Testing/06-Session_Management_Testing/02-Testing_for_Cookies_Attributes
 const cookieAttributes = {
     hostOnly: {
         warningLevel: 'high',
-        info: ''
+        info: 'This attribute prevents that the cookie is handled by other domains than the origin domain. The browser will send this cookie only to the domain that firstly sent it to the browser.'
     },
     httpOnly: {
         warningLevel: 'high',
-        info: ''
+        info: 'This attribute prevents attacks such as session leakage, since it does not allow the cookie to be accessed via a client-side script such as JavaScript.'
     },
     sameSite: {
         warningLevel: 'high',
-        info: ''
+        info: 'This attribute  used to assert that a cookie ought not to be sent along with cross-site requests. This feature allows the server to mitigate the risk of cross-origin information leakage. In some cases, it is used to prevent cross-site request forgery attacks.'
     },
     secure: {
         warningLevel: 'high',
-        info: ''
+        info: 'This attributes tells the browser to only send the cookie if the connection is HTTPS. This protects the cookie from being passed in unencrypted requests.'
     }
 };
