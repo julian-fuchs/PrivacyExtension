3e9fc481a26ce4569f0ccb40543e617897ef9958
Dominic Schaa <dominic.schaa@student.hpi.uni-potsdam.de>
2022-05-25 14:25:22 +0200

    Fix typo in trackEvent

--- js/ga-helper.js
+++ js/ga-helper.js
@@ -3,7 +3,7 @@ _gaq.push(['_setAccount', 'UA-229958247-1']);
 _gaq.push(['_trackPageview']);
 
 function clickEventHandler(e) {
-    _gaq.push(['_tackEvent', e.target.id, 'clicked']);
+    _gaq.push(['_trackEvent', e.target.id, 'clicked']);
 }
 
 function trackCheckbox() {
